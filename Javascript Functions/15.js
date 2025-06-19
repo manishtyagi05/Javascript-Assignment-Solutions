eee0fa9b4e4fadeb24d86c0a9cfd7919e0601545
Manish Tyagi <manishtyagi458@gmail.com>
2025-06-19 23:22:59 +0530

    Till 15th Function section

---
+++ Javascript Functions/15.js
@@ -0,0 +1,10 @@
+function power() {
+    const base = parseFloat(prompt("Enter the base (b):"));
+
+    const exponent = parseInt(prompt("Enter the exponent (n):"));
+  
+    const result = Math.pow(base, exponent);
+  
+    alert(`${base} raised to the power of ${exponent} is ${result}`);
+  }
+  
